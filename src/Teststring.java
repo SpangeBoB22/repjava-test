844fe7d3e9d1a45e316a72f49e19b90c588dc99d
Vitaliy <smokky.doggy@gmail.com>
2023-02-15 23:31:10 +0200

    add

---
+++ src/Teststring.java
@@ -0,0 +1,8 @@
+public class Teststring {
+    public static void main(String args[]){
+        String s1="ord";
+        String s2="ord";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.compareTo(s2));
+    }
+}
